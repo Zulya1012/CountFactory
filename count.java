4cbf1839fb5ca3671e6e248f5d6e6215cec71036
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-27 16:10:52 +0400

    Update count.java

--- count.java
+++ count.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
-public class count{
-    public static void main(String[] arg) {
-        Scanner s = new Scanner(System.in);
+public class count {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter integer number:");
-        int num = s.nextInt();
+    System.out.println("Enter integer number:");
+    int num = s.nextInt();
 
-        int count = countFactors(num);
+    int count = countFactors(num);
 
-        System.out.println(count);
+    System.out.println(count);
+  }
+
+  public static int countFactors(int num) {
+    int count = 0;
+    for (int i = 1; i <= num; ++i) {
+      if (num % i == 0) {
+        count++;
+      }
     }
-
-    public static int countFactors(int num) {
-        int count = 0;
-        for (int i = 1; i <= num; ++i) {
-            if (num % i == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
+    return count;
+  }
 }
